1e7026b667ec0ae7c8605edaba95ce433f2cee3e
Brian Kemboi <brnkemboi@gmail.com>
2024-04-02 22:32:11 +0300

    create file

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100 (inclusive)
+
+let num3 = 14
+let num4 = 5
+const mod = num3 % num4
+console.log(mod);
+
+const max = Math.max(5,10,15,20)
+console.log(max); // should output 20
